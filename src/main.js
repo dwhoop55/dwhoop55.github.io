6505535d7bf70215a670bfaa9dad3d87dacf534c
Florian Busch <busch@cs.rwth-aachen.de>
2020-11-11 21:06:08 +0100

    (feat)locale: add vue plugins

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-// import i18n from "./i18n";
+import i18n from './i18n'
 // import router from './router'
 
 import { init } from 'emailjs-com';
@@ -27,7 +27,7 @@ files.keys().map(key =>
 )
 
 new Vue({
+  i18n,
   // router,
-  // i18n,
   render: h => h(App)
 }).$mount('#app')
